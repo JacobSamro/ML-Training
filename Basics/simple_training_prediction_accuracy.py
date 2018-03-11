66c9f93b62983124c3d49149fde880002413f56e
Jacob Samro <JacobSamro@users.noreply.github.com>
2018-03-11 15:41:36 +0530

    Create simple_training_prediction_accuracy.py

---
+++ Basics/simple_training_prediction_accuracy.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+import random
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+### Compute the accuracy of your Naive Bayes classifier
+### import the sklearn module for GaussianNB
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+#TODO
+clf.fit(features_train,labels_train)
+
+### use the trained classifier to predict labels for the test features
+pred = clf.predict(features_test)
+
+
+### calculate and return the accuracy on the test data
+accuracy = accuracy_score(pred,labels_test)
+print(accuracy)
+
+
+def makeTerrainData(n_points=1000):
+###############################################################################
+### make the toy dataset
+    random.seed(42)
+    grade = [random.random() for ii in range(0,n_points)]
+    bumpy = [random.random() for ii in range(0,n_points)]
+    error = [random.random() for ii in range(0,n_points)]
+    y = [round(grade[ii]*bumpy[ii]+0.3+0.1*error[ii]) for ii in range(0,n_points)]
+    for ii in range(0, len(y)):
+        if grade[ii]>0.8 or bumpy[ii]>0.8:
+            y[ii] = 1.0
+
+### split into train/test sets
+    X = [[gg, ss] for gg, ss in zip(grade, bumpy)]
+    split = int(0.75*n_points)
+    X_train = X[0:split]
+    X_test  = X[split:]
+    y_train = y[0:split]
+    y_test  = y[split:]
+
+    grade_sig = [X_train[ii][0] for ii in range(0, len(X_train)) if y_train[ii]==0]
+    bumpy_sig = [X_train[ii][1] for ii in range(0, len(X_train)) if y_train[ii]==0]
+    grade_bkg = [X_train[ii][0] for ii in range(0, len(X_train)) if y_train[ii]==1]
+    bumpy_bkg = [X_train[ii][1] for ii in range(0, len(X_train)) if y_train[ii]==1]
+
+#    training_data = {"fast":{"grade":grade_sig, "bumpiness":bumpy_sig}
+#            , "slow":{"grade":grade_bkg, "bumpiness":bumpy_bkg}}
+
+
+    grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==1]
+    bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==1]
+
+    test_data = {"fast":{"grade":grade_sig, "bumpiness":bumpy_sig}
+            , "slow":{"grade":grade_bkg, "bumpiness":bumpy_bkg}}
+
+    return X_train, y_train, X_test, y_test
